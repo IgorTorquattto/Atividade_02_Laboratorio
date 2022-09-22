9f79c3fd8928949e1e3d054ae42224946249b93f
igor <igor@gmail.com>
2022-09-22 09:07:58 -0300

    Arquivos

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Digite o número da questão que você deseja executar: [1,6]"
+read $questao
+case $questao in
+1) ./ex1.sh ;;
+2) echo "dois" ;;
+3) echo "tres" ;;
+4) echo "quatro" ;;
+5) echo "cinco" ;;
+6) echo "seis" ;;
+7) echo "sete" ;;
+8) echo "oito" ;;
+9) echo "nove" ;;
+10) echo "dez" ;;
+esac
