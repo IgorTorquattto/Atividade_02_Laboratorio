d2333b66b2b83f66d4dd6c57770e74f96b9c1b72
erickxbliv <eriplayck@gmail.com>
2022-10-01 14:09:32 -0300

    eu permiti que a questão 7 receba parametros

--- ex7.sh
+++ ex7.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-data1=$(date -d "$1" +%s)
-data2=$(date -d "$2" +%s)
+echo "por favor, me informe o intervalo das aulas. Qual o primeiro dia de aula? (ano completo/mes/dia): "
+read PRIMEIRA
+echo "certo. Qual a data do ultimo dia de aula (no mesmo formato)? "
+read ULTIMA
+echo "Agora.. me informe quais os dias em que existe aula (exemplo: 1,3 sendo 1 a segunda)"
+read QDIAS
+data1=$(date -d "$PRIMEIRA" +%s)
+data2=$(date -d "$ULTIMA" +%s)
 
 for ((i=$data1; i<=$data2; i+=86400));
 do
   data=$(date -d @$i +%w)
-  if [[ $3 =~ $data ]]
+  if [[ $QDIAS =~ $data ]]
   then
   	echo "$(date -d @$i +%d/%m/%Y)"
   fi
